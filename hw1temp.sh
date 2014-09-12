cc022adac01b265fe05c3181cb1bd5883ee3b934
Andres <marmot91@ksu.edu>
2014-09-12 16:00:52 -0500

    This is the change to allow the temperature to be output in Kelvin

---
+++ hw1temp.sh
@@ -0,0 +1,13 @@
+#bin/bash
+
+echo "Enter the temperature in Farenheit.";
+read temp;
+
+let sub=$temp-32; #Calculations for C
+let C=$sub*5/9;
+let K=$C+273; #Calculation for K
+
+echo "The temperature in Celsius is $C."
+echo "The temperature in Kelvin is $K." #Change to state temperature in Kelvin
+
+
